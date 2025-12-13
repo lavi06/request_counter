7b699deb2c0cde2097e5f0e1845b19176c8b5f17
Himanshu Goyal <hggoyal06@gmail.com>
2025-12-13 15:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,27 +11,31 @@ import requests
 # Config
 # -------------------------
 FLASK_API_BASE = "https://1grabintel.pythonanywhere.com"  # your running Flask app
+# FLASK_API_BASE = "http://127.0.0.1:5000"  # your running Flask app
+
+
 st.set_page_config(layout="wide")
 
 
 # ---- Set your secret API KEY here ----
-VALID_API_KEY = st.secrets["VALID_API_KEY"]
+VALID_API_KEY = "c6ce28200413a4f4a0858401e7f5470c207d50554210cbbfc984ab0197cd6617"
+# VALID_API_KEY = st.secrets("VALID_API_KEY")
+
+
 # ---- Read API key from URL params ----
 api_key = st.api_key = st.query_params.get("api_key", "")
+
 
 # ---- Authentication ----
 if api_key != VALID_API_KEY:
     st.error("❌ Unauthorized. Please provide a valid api_key in the URL.")
     st.stop()
 
-CLIENT_API_KEY = api_key
 
+CLIENT_API_KEY = VALID_API_KEY
+
+# ##### LOGIC TO ADD CREDITS
 # credit_url = f"{FLASK_API_BASE}/add_credits"
-
-# # billing_url = f"{FLASK_API_BASE}/billing_log?api-key={CLIENT_API_KEY}&last_n=50"
-# # resp = requests.get(billing_url)
-# # print(resp.content)
-# # c
 # data = {"api_key" : CLIENT_API_KEY, "credits" : 1000, "billing_reference" : "Test"}
 # resp = requests.post(credit_url, json = data)
 # print(resp.content)
@@ -70,27 +74,32 @@ with usage_tab:
 
     if usage_data:
         num_requests = usage_data.get("total_requests", 0)
-        # st.metric("Requests Since Last Payment", num_requests)
 
         st.subheader("Last 50 Requests")
         last_requests = usage_data.get("last_50_requests", [])
         if last_requests:
 
             df = pd.DataFrame(last_requests)
-            df = df.drop(["api_key","id"], axis = 1)
+
+
 
             df['UTC Query Time'] = pd.to_datetime(df['request_datetime']).dt.strftime("%b %d %Y, %I:%M %p")
 
-            df = df.drop(["request_datetime", "response_content"], axis = 1)
 
-            col = "UTC Query Time"
-            df = df[[col] + [c for c in df.columns if c != col]]
+            for col in ["api_key", "id", "request_datetime", "response_content"]:
+                try: df = df.drop([col], axis = 1)
+                except: pass
+
+
+            col = ["UTC Query Time", "credit_deducted","website", "query_params", "response_status"]
+            df = df[col + [c for c in df.columns if c not in col]]
 
 
             column_config = {col: {'alignment': 'center'} for col in df.columns}
             st.dataframe(df, column_config= column_config)
         else:
             st.write("No recent requests available.")
+
 
 
 with credit_tab:
@@ -125,5 +134,23 @@ with credit_tab:
             st.write("No recent requests available.")
 
 
+# # -------------------------
+# # 2. Payment Simulation
+# # -------------------------
+# st.subheader("Mark Payment")
 
+# payment_amount = st.number_input("Payment Amount ($)", min_value=0.0, value=10.0, step=1.0)
 
+# if st.button("Confirm Payment"):
+#     try:
+#         # Call Flask API to record payment
+#         payment_url = f"{FLASK_API_BASE}/record_payment"
+#         payload = {
+#             "api_key": CLIENT_API_KEY,
+#             "amount_paid": payment_amount
+#         }
+#         pay_resp = requests.post(payment_url, json=payload)
+#         pay_resp.raise_for_status()
+#         st.success("Payment recorded successfully!")
+#     except Exception as e:
+#         st.error(f"Failed to record payment: {e}")
