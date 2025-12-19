3ad0e8d1b66124550a4fa39da859aaff10fcb19c
Himanshu Goyal <hggoyal06@gmail.com>
2025-12-20 02:28:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(layout="wide")
 
 
 # ---- Set your secret API KEY here ----
-VALID_API_KEY = st.secrets("VALID_API_KEY")
+VALID_API_KEY = st.secrets["VALID_API_KEY"]
 
 
 # ---- Read API key from URL params ----
